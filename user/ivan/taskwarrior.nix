ff70894763674bc6dc79c47a73b3666b1a37453a
Ivan Petkov <ivanppetkov@gmail.com>
2020-12-20 19:50:50 -0800

    Don't enable taskwarrior via home-manager
    * home-manager insists on placing a .taskrc link within the root of the
      $HOME directory, which does not play nicely if $HOME is not
      writable...
    * taskwarrior does allow for pointing towards another location of
      .taskrc via the TASKRC environment variable, but home-manager does not
      appear to support or work with this
    * To avoid any headaches, we'll define the config here outselves

--- user/ivan/taskwarrior.nix
+++ user/ivan/taskwarrior.nix
@@ -1,8 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
+  home.sessionVariables = {
+    TASKRC = "${config.programs.taskwarrior.dataLocation}/taskrc";
+  };
+
   home.packages = with pkgs; [
     tasksh
+    # NB: do not use the home-manager version of task warrior
+    # since it insists on placing the .taskrc file in $HOME
+    taskwarrior
   ];
-
-  programs.taskwarrior.enable = true;
 }
