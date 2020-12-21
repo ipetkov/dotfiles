77061343565718b73b557b835d5d745e0f503c27
Ivan Petkov <ivanppetkov@gmail.com>
2020-12-20 19:59:41 -0800

    Add fish prompt to distinguish when inside of a nix-shell

--- user/ivan/fish.nix
+++ user/ivan/fish.nix
@@ -9,6 +9,20 @@
       end
     '';
 
+    promptInit = ''
+      if test -n "$IN_NIX_SHELL"
+        set --global nix_shell_info "<nix-shell> "
+      else
+        set --global nix_shell_info ""
+      end
+
+      functions --copy fish_prompt fish_prompt_default
+      function fish_prompt
+        echo -n -s "$nix_shell_info"
+        fish_prompt_default
+      end
+    '';
+
     # NB: allow nvim to create its own alias/symlink if enabled
     shellInit = "set -x EDITOR vim";
 
