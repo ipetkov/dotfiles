47c48678d7fe0dc044af3586aba6e07940c887c5
Ivan Petkov <ivanppetkov@gmail.com>
2021-01-12 21:00:31 -0800

    Enable ssh-agent

--- nixosConfigurations/tartarus/default.nix
+++ nixosConfigurations/tartarus/default.nix
@@ -81,7 +81,7 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
+  programs.ssh.startAgent = true;
   programs.gnupg.agent = {
     pinentryFlavor = "curses";
     enable = true;
