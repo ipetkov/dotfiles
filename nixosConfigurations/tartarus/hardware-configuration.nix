4e10973b3101926a36e7299c1f02e88e30c4cd14
Ivan Petkov <ivanppetkov@gmail.com>
2021-01-17 13:42:23 -0800

    deps: update
    * Also remove the nonexistent aes_x86_64 kernel module from the
      available boot modules, turns out this module doesn't exist (I had
      originally copied it from the wiki stating that this would speed up
      the luks decryption on boot, seems like this is probably available in
      more recent kernels without needing to load a separate module)

--- nixosConfigurations/tartarus/hardware-configuration.nix
+++ nixosConfigurations/tartarus/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "aes_x86_64" "cryptd" "amdgpu" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "cryptd" "amdgpu" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
