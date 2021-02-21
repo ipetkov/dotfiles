1b724162d390ab4a44a64000c21925867e368382
Ivan Petkov <ivanppetkov@gmail.com>
2021-02-21 09:22:27 -0800

    rust module: fix Linux check to work with flakes
    * Flakes don't allow access to `builtins.currentSystem` since that may
      not be reproducible
    * Use `pkgs.stdenv.isLinux` instead of the raw lib functions directly

--- homeManagerModules/rust.nix
+++ homeManagerModules/rust.nix
@@ -1,9 +1,4 @@
 { config, pkgs, lib, ... }:
-let
-  inherit (lib.systems);
-  system = lib.systems.parse.mkSystemFromString builtins.currentSystem;
-  isLinux = lib.systems.inspect.predicates.isLinux system;
-in
 {
   home.sessionVariables = {
     RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
@@ -16,5 +11,5 @@ in
     clang # Provides `cc` for any *-sys crates
     rust-analyzer
     rustup
-  ] ++ lib.lists.optional isLinux pkgs.binutils; # For some reason conflicts on darwin
+  ] ++ lib.lists.optional pkgs.stdenv.isLinux pkgs.binutils; # For some reason conflicts on darwin
  }
