ea10f132031ff494ff2de7491d64b2f71a9e5186
Ivan Petkov <ivanppetkov@gmail.com>
2021-10-25 15:18:02 -0700

    tartarus: enable aarch64-linux binfmt
    * This will allow us to easily build aarch64-linux images without
      needing to mess around with cross compilation directly

--- nixosConfigurations/tartarus/default.nix
+++ nixosConfigurations/tartarus/default.nix
@@ -8,6 +8,8 @@
       ../../users/ops.nix
     ];
 
+   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
