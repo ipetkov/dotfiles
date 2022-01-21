d44b1a9b5eee7b4965ba23fbb3568f57a4211699
Ivan Petkov <ivanppetkov@gmail.com>
2022-01-20 20:46:02 -0800

    fzf: add changeDirWidgetCommand
    * This allows for quickly changing directories with ALT-C and fzf
      goodness

--- homeManagerModules/fzf.nix
+++ homeManagerModules/fzf.nix
@@ -4,10 +4,12 @@
     enable = true;
     defaultCommand = "rg --files --hidden --glob !.git";
     fileWidgetCommand = "rg --files --hidden --glob !.git";
+    changeDirWidgetCommand = "fd --type d";
   };
 
   home.packages = with pkgs; [
-    # Ensure we include ripgrep for defaultCommand above
+    # Ensure we include ripgrep and fd for the commands above
     ripgrep
+    fd
   ];
  }
