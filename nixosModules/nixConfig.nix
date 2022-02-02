af184d05ab3c88a451b10ec4b7a61afa1561b4f7
Ivan Petkov <ivanppetkov@gmail.com>
2022-02-02 15:54:16 -0800

    Revert "nixosModules: nixConfig: rename deprecated options"
    This reverts commit a0e0193a1d24f8e0b857dc33ac9b655116269f55.

--- nixosModules/nixConfig.nix
+++ nixosModules/nixConfig.nix
@@ -3,6 +3,7 @@
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";
+    useSandbox = true;
 
     # Use our inputs as defaults for nixpkgs/nixos so everything (like nix-env)
     # moves in lockstep. (Note adding a channel will take precedence over this).
@@ -13,13 +14,10 @@
       nixpkgs.flake = inputs.nixpkgs;
     };
 
-    settings = {
-      sandbox = true;
-      substituters = ["https://ipetkov.cachix.org"];
-      trusted-public-keys = [
-        "ipetkov.cachix.org-1:xK9taxnomX0ZVyDmobpZB5AQvuZ+L3q4u7IlRvEtomg="
-      ];
-    };
+    binaryCaches = ["https://ipetkov.cachix.org"];
+    binaryCachePublicKeys = [
+      "ipetkov.cachix.org-1:xK9taxnomX0ZVyDmobpZB5AQvuZ+L3q4u7IlRvEtomg="
+    ];
 
     gc = {
       automatic = true;
