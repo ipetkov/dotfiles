7d664a8ec6f1d223b55ed7c1ba51bc10ea40f5e4
Ivan Petkov <ivanppetkov@gmail.com>
2022-05-12 16:19:18 -0700

    tailscale: update firewall rules
    Add `networking.firewall.checkReversePath = "loose"` as per evaluation
    warning

--- nixosModules/tailscale.nix
+++ nixosModules/tailscale.nix
@@ -52,6 +52,7 @@ in
         # Needed for taildrop to work, may need to be revisited in the future
         # https://forum.tailscale.com/t/taildrop-not-working-when-sending-a-file-from-iphone-to-a-nixos-machine/633/7
         trustedInterfaces = [ cfg.interfaceName ];
+        checkReversePath = "loose";
       };
     }
   ]);
