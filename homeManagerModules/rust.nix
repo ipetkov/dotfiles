5781dbbb982531f55dcf0f75b40777bb35cd2b62
Ivan Petkov <ivanppetkov@gmail.com>
2023-05-24 17:08:17 -0700

    rust: remove rust-analyzer proxy from rustup

--- homeManagerModules/rust.nix
+++ homeManagerModules/rust.nix
@@ -9,7 +9,14 @@
     cargo-outdated
     cargo-update
     rust-analyzer
-    rustup
+
+    # rustup now carries a rust-analyzer proxy which collides with our direct rust-analyzer install
+    (pkgs.runCommand "rustup-no-rust-analyzer-proxy" { } ''
+      cp -rs ${rustup} $out
+      find $out
+      rm -f $out/bin/rust-analyzer
+      find $out
+    '')
   ] ++ lib.lists.optionals pkgs.stdenv.isLinux (with pkgs; [
     # binutils now conflicts with clang as well, turning this off for now...
     # binutils # For some reason conflicts on darwin
