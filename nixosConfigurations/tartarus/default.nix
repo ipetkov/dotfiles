8a9a28710992096b91fd6cd12f5efa2688eeb4d4
Ivan Petkov <ivanppetkov@gmail.com>
2023-09-10 13:12:26 -0700

    tartarus: start the polkit agent via systemd, not sway

--- nixosConfigurations/tartarus/default.nix
+++ nixosConfigurations/tartarus/default.nix
@@ -156,4 +156,18 @@
   programs.command-not-found.enable = false;
 
   dotfiles._1password.enable = true;
+
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
 }
