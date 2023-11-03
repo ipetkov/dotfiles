e186301a4c92536037a2f75fff140bdbffdd2844
Ivan Petkov <ivanppetkov@gmail.com>
2023-11-03 11:43:06 -0700

    ci: switch to using nix-fast-build

--- ci/common.nix
+++
@@ -1,9 +0,0 @@
-let
-  getFlakeCompat = path: import path;
-  selectGetFlake = { getFlake ? getFlakeCompat, ...}: getFlake;
-  getFlake = selectGetFlake builtins;
-  flake = getFlake (toString ./..);
-in
-{
-  inherit flake;
-}
