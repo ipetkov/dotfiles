c8e0c0a70d788427b9afcb74d0b14cb4dcbc6996
Ivan Petkov <ivanppetkov@gmail.com>
2024-03-19 14:49:31 -0700

    tartarus: enable memtest86

--- nixosConfigurations/tartarus/default.nix
+++ nixosConfigurations/tartarus/default.nix
@@ -11,6 +11,7 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
   # https://nixos.wiki/wiki/NixOS_on_ZFS
   boot.loader.grub.copyKernels = true;
