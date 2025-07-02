8bbdfe4b7487f8f3e95a7366e2892b43d349b5f4
Ivan Petkov <ivanppetkov@gmail.com>
2025-07-02 15:40:02 -0700

    rpi: bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     # mixing and matching branches.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # Pinned because deploying to rpi is slow as molasses due to SD card I/O being crap
-    nixpkgs-for-rpi.url = "github:NixOS/nixpkgs/4396a137499b6cc9f9fe9f3c266577bd52d455a4";
+    nixpkgs-for-rpi.url = "github:NixOS/nixpkgs/3016b4b15d13f3089db8a41ef937b13a9e33a8df";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     flake-compat = {
