c802e8f30bf4489a8451588a5a6e8b493cc17160
Ivan Petkov <ivanppetkov@gmail.com>
2025-09-19 13:58:17 -0700

    tailscale: drop pin

--- nixosModules/tailscale.nix
+++ nixosModules/tailscale.nix
@@ -1,30 +1,10 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, ... }:
 let
   inherit (lib) mkIf;
   cfg = config.services.tailscale;
-
-  # https://github.com/NixOS/nixpkgs/pull/442245
-  # https://nixpk.gs/pr-tracker.html?pr=442245
-  pinned = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/8eb28adfa3dc4de28e792e3bf49fcf9007ca8ac9.tar.gz";
-    sha256 = "sha256-NOrUtIhTkIIumj1E/Rsv1J37Yi3xGStISEo8tZm3KW4=";
-  };
-
-  chosenPkgs =
-    if pkgs.tailscale.version == "1.86.4" then
-      import pinned { inherit (config.nixpkgs) system; }
-    else
-      pkgs;
 in
 {
   config = mkIf cfg.enable {
-    services.tailscale.package = chosenPkgs.tailscale;
-
     networking.firewall = {
       allowedUDPPorts = [ cfg.port ];
       # Needed for taildrop to work, may need to be revisited in the future
