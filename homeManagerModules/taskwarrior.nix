b0222c2e446d1abe43f400027c76b80cf72db09f
Ivan Petkov <ivanppetkov@gmail.com>
2025-10-12 15:40:40 -0700

    taskwarrior: fix build

--- homeManagerModules/taskwarrior.nix
+++ homeManagerModules/taskwarrior.nix
@@ -12,7 +12,14 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = [
-      pkgs.tasksh
+      (pkgs.tasksh.overrideAttrs (old: {
+        # https://github.com/NixOS/nixpkgs/pull/451341
+        # https://nixpk.gs/pr-tracker.html?pr=451341
+        cmakeFlags = (old.cmakeFlags or [ ]) ++ [
+          "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+        ];
+      }))
+
       # NB: do not use the home-manager version of task warrior
       # since it insists on placing the .taskrc file in $HOME
       pkgs.taskwarrior3
